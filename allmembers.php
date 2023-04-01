cb0fb21c098a415e62ef64563f3f9d581b6c83fb
Md Sayed Hossen <127623616+shbablu23@users.noreply.github.com>
2023-04-01 16:24:56 +0600

    Add files via upload

---
+++ allmembers.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+//print_r($_SESSION['form_error']);
+
+include './env.php';
+
+$query= "SELECT * FROM addresses";
+$response=mysqli_query($dbconnect,$query);
+//mysqli_fetch_all($response);
+$alladdresses=mysqli_fetch_all($response,1); //1=true, array convert to assciative array
+// echo "<pre>";
+// print_r($alladdresses);
+// echo "</pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>submit form</title>
+    <!-- bootstrap css file -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+  <div class="container col-lg-6 mx-auto mt-2 bg-dark text-light text-center w=100">
+    <h3>Home work submission</h3>
+  </div>
+
+
+  <nav class="navbar navbar-expand-lg navbar" style="background-color: #e3f2fd;">
+    <div class="container">
+      <a class="navbar-brand" href="#">Address Book</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav m-auto mb-2 mb-lg-0 w=100">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./submitform.php">Add new</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./allmembers.php">All members</a>
+        </li>
+      </div>
+    </div>
+  </nav>
+  <div class="col-lg-8 mx-auto mt-5">
+    <div>
+      <table class="table table-responsive">
+        <tr>
+          <th>SL no</th>
+          <th>name</th>
+          <th>address</th>
+          <th>phone</th>
+          <th>email</th>
+          <th>review</th>
+          <th>actions</th>
+        </tr>
+
+        <?php //dynamic vabe sob data add hobe
+        foreach($alladdresses as $index=> $address){
+          ?>
+          
+          <tr>
+            <!-- (++$index) serial start from Zero(0) -->
+            <td><?= ++$index ?></td> 
+            <td><?=!empty($address['name']) ? $address['name'] : 'USER'?></td> 
+            <td><?= $address['address'] ?></td> 
+            <td><?= $address['phone'] ?></td>
+            <td><?= $address['email'] ?></td>
+            <td><?= $address['review'] ?></td>
+            <td>
+              view
+              edit
+              delete
+            </td>
+        </tr>
+
+        <?php
+        }
+
+        ?>
+        
+      </table>
+    </div>
+
+  </div>
+  
+
+
+
+
+
+
+</body>
+</html>
+
+<?php
+session_unset();
+?>
